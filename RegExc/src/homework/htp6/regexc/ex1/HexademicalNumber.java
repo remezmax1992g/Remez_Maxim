1a5e9d36c4368fe2daa6e2bbe1bd24a0362d17a8
remez <remez@DESKTOP-85IN2NM>
2016-10-30 23:44:28 +0300

    New Version

--- RegExc/src/homework/htp6/regexc/ex1/HexademicalNumber.java
+++ RegExc/src/homework/htp6/regexc/ex1/HexademicalNumber.java
@@ -1,10 +1,40 @@
 package homework.htp6.regexc.ex1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HexademicalNumber {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private final static String regexc = "0[xX](([\\da-f]+)|([\\dA-F]+))";
+	private  List<String> listHexNumber;
+	
+	public HexademicalNumber(){
+		this.listHexNumber = new ArrayList<String>();
+	}
+	
+	public static String getRegexc(){
+		
+	return regexc;
+	
+	}
+	
+	public void set(String element){
+		
+		listHexNumber.add(element);
+		
+	}
+	
+	public List<String> getListHexNumber(){
+		
+		return listHexNumber;
+	}
+	
+	public void print(List<String> list){
+		System.out.println("Все найденные числа в тексте записанные в шестиричной системе счисления");
+		for (int i = 0; i < list.size(); i++){
+			
+		System.out.println(list.get(i));
+		}
 	}
 
 }
---
+++ RegExc/src/homework/htp6/regexc/ex1/TestHexademicalNumber.java
@@ -0,0 +1,30 @@
+package homework.htp6.regexc.ex1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestHexademicalNumber {
+
+	public static void main(String[] args) throws IOException {
+		HexademicalNumber h = new HexademicalNumber();
+		String regexc = h.getRegexc();
+		System.out.println("¬ведите текст дл€ поиска");
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		String text = r.readLine();
+		Pattern p = Pattern.compile(regexc);
+		Matcher m = p.matcher(text);
+		
+		while (m.find()){
+			h.set(m.group());
+		}
+		
+		List<String> list = h.getListHexNumber();
+		h.print(list);
+
+	}
+
+}
