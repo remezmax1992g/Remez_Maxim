1f72e8fd552a0ac3c33abcc46e6145a9b4c3f4e0
remez <remez@DESKTOP-85IN2NM>
2016-11-20 23:18:02 +0300

    q

--- RegExc/src/homework/htp6/regexc/ex2/Mail.java
+++ RegExc/src/homework/htp6/regexc/ex2/Mail.java
@@ -2,7 +2,7 @@ package homework.htp6.regexc.ex2;
 
 public class Mail {
 	
+	private final static String regexc = "[A-Za-z";
     public static void main(String[] args){
-	System.out.println("привет");
     }
 }
